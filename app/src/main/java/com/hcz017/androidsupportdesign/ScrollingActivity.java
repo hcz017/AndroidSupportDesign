32938655447b4ad88b6849d5c2fd09ec532da0be
chengzhi.hou <hcz017@gmail.com>
2017-04-11 19:50:30 +0800

    新增CollapsingToolBarLayout
    折叠布局要作为AppBarLayout的直接子布局使用，作为Toolbar的
    包装器实现滑动app bar的特点。有设置title样式，设置收缩后
    内容纱布，状态栏纱布等特点，详见代码。

--- app/src/main/java/com/hcz017/androidsupportdesign/ScrollingActivity.java
+++ app/src/main/java/com/hcz017/androidsupportdesign/ScrollingActivity.java
@@ -1,6 +1,8 @@
 package com.hcz017.androidsupportdesign;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -8,6 +10,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 public class ScrollingActivity extends AppCompatActivity {
 
@@ -16,7 +19,10 @@ public class ScrollingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scrolling);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //toolbar 作为actionBar使用 style里面已经隐藏了actionBar(title也隐藏了)
         setSupportActionBar(toolbar);
+        //不会生效，Toolbar被CollapsingToolbarLayout包裹着,要设置collapsingToolbarLayout的title
+//        toolbar.setTitle("toolbar");
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -26,6 +32,32 @@ public class ScrollingActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        customUI();
+    }
+
+    /**
+     * 自定义显示效果
+     */
+    public void customUI(){
+//        设置标题
+//        this.setTitle(R.string.title);
+        CollapsingToolbarLayout collapsingToolbarLayout =
+                (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
+        collapsingToolbarLayout.setTitle(getString(R.string.title));
+//        扩张时title的颜色
+        collapsingToolbarLayout.setExpandedTitleColor(Color.WHITE);
+//        收缩后title的颜色
+        collapsingToolbarLayout.setCollapsedTitleTextColor(Color.RED);
+//        动态修改背景，如果不设置image的话可以fvb找到toolbar或者CollapsingToolbarLayout设置背景
+//        findViewById(R.id.img_scroll_bg).setBackgroundResource(R.drawable.ic_menu_camera);
+//        图片点击效果
+        findViewById(R.id.img_scroll_bg).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ScrollingActivity.this, "You just clicked the image", Toast.LENGTH_SHORT).show();
+            }
+        });
+//        collapsingToolbarLayout.setVisibility(View.INVISIBLE);
     }
 
     @Override
